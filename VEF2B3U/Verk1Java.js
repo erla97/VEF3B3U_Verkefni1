57f31bc572c2175b84c62f4f920d448445a39f91
erla97 <erlaos207@nemi.tskoli.is>
2017-08-24 12:21:34 +0000

    Add files via upload

---
+++ VEF2B3U/Verk1Java.js
@@ -0,0 +1,15 @@
+document.write("Halló Heimur");
+
+var word;
+var number1;
+var number2;
+
+word = "Tommustokkur";
+number1 = 10;
+number2 = 6;
+
+var text = document.getElementById('text');
+text.textContent = word;
+
+var multiply = document.getElementById('total');
+multiply.textContent = number1 * number2;
