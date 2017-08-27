f3bde02a5c98abb4b965c1ce6d48293bb8a4ca2f
erla97 <erlaos207@nemi.tskoli.is>
2017-08-27 18:40:06 +0000

    Klárað

--- VEF2B3U/Verk1Java.js
+++ VEF2B3U/Verk1Java.js
@@ -4,7 +4,7 @@ var word;
 var number1;
 var number2;
 
-word = "Tommustokkur";
+word = "Market";
 number1 = 10;
 number2 = 6;
 
@@ -13,3 +13,9 @@ text.textContent = word;
 
 var multiply = document.getElementById('total');
 multiply.textContent = number1 * number2;
+
+var shopping = ['apple', 'milk', 'cheese', 'bread', 'coffee'];
+
+for (i = 0; i < shopping.length; i++) {
+    document.getElementById("shopinglist").innerHTML += shopping[i] + "<br>";
+}
